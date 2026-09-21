c94656996d45678fbd7adf0e284cd8eeb162b5ee
jerycort35 <331794889+jerycort35@users.noreply.github.com>
2026-09-21 04:04:16 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
+compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
  namespace="fr.leslapibreizh.mediatheque"; compileSdk=35
  defaultConfig { applicationId="fr.leslapibreizh.mediatheque"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1.0" }
 }
